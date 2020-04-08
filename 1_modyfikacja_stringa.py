4749eaf7b312fa7a3b7e18ee632073d57d14bca8
Marta Potocka <marta.m.potocka@gmail.com>
2020-04-08 20:40:00 +0200

    Update

--- 1_modyfikacja_stringa.py
+++ 1_modyfikacja_stringa.py
@@ -3,12 +3,13 @@
 
 a = "abcdefg"
 print(a[1])
-# a[1] = 'B' # próba modyfikacji stringa skutkuje wypisaniem TypeError
+# a[1] = 'X' # próba modyfikacji stringa skutkuje wypisaniem TypeError
 print(a)
 
-# Jeśli chcemy zmodyfikować string, najłatwiej zrobić to zamieniając go na chwilę w listę
+# Jeśli chcemy zmienić zawratość stringa, musimy utworzyć nowy string
+# który przypiszemy do zmiennej zawierającej pierwotny string
 
 a_lista = list(a)     # tworzenie listy zawierającej poszczególne litery stringa
-a_lista[1] = 'B'      # modyfikacja elementu listy (modyfikowanie list jest legalne!)
+a_lista[1] = 'X'      # modyfikacja elementu listy (modyfikowanie list jest legalne!)
 a = "".join(a_lista)  # tworzenie nowego stringa z elementów listy
 print(a)
