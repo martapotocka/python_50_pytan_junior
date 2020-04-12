8309e194c76afc392604a4b3c8def3b1b4fc07b5
Marta Potocka <marta.m.potocka@gmail.com>
2020-04-12 14:30:11 +0200

    Update

--- 3_roznica_lista_tupla.py
+++ 3_roznica_lista_tupla.py
@@ -1,20 +1,10 @@
-# Pytanie 3 - czym różni się lista od tupli
+# Pytanie 3 - napisz kod, który zaprezentuje rożnicę między listą a tuplą
 
-# zacząć od nazewnictwa, że tupla to spolszczenie angielskiego tuple
-# i poprawnie to jest krotka, ale mało osób tak mówi
+# Lista od tupli różni się zapisem
+A = [1,2,3]  # listę zapisujemy w nawiasach kwadratowych
+C = (1,2,3)  # tuplę zapisujemy w nawiasach okrągłych
 
-# Powiedzieć, że listy i tuple mają wiele wspólnego, mogą przechowyać
-# różne typy danych, mogą być zagnieżdżone, w identyczny sposób
-# odczytujemy elementy
-
-# powiedzieć, że różnica jet w zapisie [] vs ()
-# oraz w mutowalności, bo tuple są niemutowalne
-
-A = [1,2,3]
-B = [1, True, 'jakis_string', (4,5,6)]
-C = (1,2,3)
-D = (2, False, 'xxx', [7,8,9])
-
-print(A[0], B[1], C[2], D[0])
-A[0] = 111
-C[0] = 111
+# Lista jest mutowalna (modyfikowalna), a tupla nie!
+# Najłatwiej to pokazać próbując zmodyfikować elementy obu struktur danych
+A[0] = 111   # modyfikacja listy jest dopuszczalna i uda się
+C[0] = 111   # modyfikacja tupli nie jest dupuszczalna i spowoduje wypisanie TypeError!
