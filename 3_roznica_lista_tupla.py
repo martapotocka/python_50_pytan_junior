92e96330327c5d4732fe1a4a71f796bb5ab0180d
Marta Potocka <marta.m.potocka@gmail.com>
2020-04-12 14:40:22 +0200

    Update

--- 3_roznica_lista_tupla.py
+++ 3_roznica_lista_tupla.py
@@ -1,15 +1,6 @@
 # Pytanie 3 - napisz kod, który zaprezentuje
 # najważniejsze różnice między listą a tuplą
 
-# zacząć od nazewnictwa, że tupla to spolszczenie angielskiego tuple
-# i poprawnie to jest krotka, ale mało osób tak mówi
-
-# Powiedzieć, że listy i tuple mają wiele wspólnego, mogą przechowyać
-# różne typy danych, mogą być zagnieżdżone, w identyczny sposób
-# odczytujemy elementy
-
-# powiedzieć, że różnica jet w zapisie [] vs ()
-# oraz w mutowalności, bo tuple są niemutowalne
 
 # Lista od tupli różni się zapisem
 A = [1,2,3]  # listę zapisujemy w nawiasach kwadratowych
