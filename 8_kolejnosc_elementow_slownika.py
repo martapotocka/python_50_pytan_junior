6f989c2c85690aa519eaa721fdaa4eee3a330924
Marta Potocka <marta.m.potocka@gmail.com>
2020-04-13 17:29:46 +0200

    Update

---
+++ 8_kolejnosc_elementow_slownika.py
@@ -0,0 +1,9 @@
+# Pytanie 8 - Co zostanie wypisane w wyniku wykonania poniższego kodu?
+
+D = {1: 'Ala', 2: 'ma', 3: 'kota'}
+
+# Pętla for iteruje zawsze po kluczach słownika
+for key in D:       # dla kolejnego klucza w słowniku D
+    print(D[key])   # wydrukuj wartość przechowywaną pod tym kluczem
+
+# Odpowiedź: nie wiadomo! Słowniki nie trzymają kolejności!
