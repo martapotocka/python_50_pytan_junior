dde0ed26bd61fc441af8bf7c6037ab72fb4aa256
Marta Potocka <marta.m.potocka@gmail.com>
2020-04-13 15:59:26 +0200

    Update

---
+++ 7_co_moze_byc_kluczem_slownika.py
@@ -0,0 +1,11 @@
+# Pytanie 7 - które z poniższych słowników zostały utworzone niepoprawnie?
+# Dlaczego?
+
+A = {1: 1, 2: 4, 3: 9}
+B = {'imie': 'Anna', 'nazwisko': 'Kowalska'}
+C = {[4, 5]: [16, 25]}
+D = {(4, 5): [16, 25]}
+E = {{1:2}: 'jeden_dwa'}
+
+# Pokazac ze mamy TypeError: unhashable type: list/dict
+# nawiązać do poprzedniej lekcji
